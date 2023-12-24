9f330a97e5d8c2fdbe341146e40934cdbc020864
Cleiton Machado <contato@cleitonmachado.com>
2023-12-24 01:10:14 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
-﻿using DesafioPOO.Models;
+﻿using System.Formats.Asn1;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia n1 = new("123", "a1", "123456", 32);
+
+Console.WriteLine("Testes Nokia\n");
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("WhatsApp");
+Console.WriteLine("-----------------\n");
+Iphone p1 = new("123", "a1", "123456", 32);
+
+Console.WriteLine("Testes IPhone\n");
+p1.Ligar();
+p1.ReceberLigacao();
+p1.InstalarAplicativo("WhatsApp");
+
+
